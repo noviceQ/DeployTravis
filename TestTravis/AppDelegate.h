397ad822a795c55969e82db4634e3f4eb40681d9
superfls <lincappu@163.com>
2017-05-04 22:25:39 +0800

    Initial Commit

---
+++ TestTravis/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestTravis
+//
+//  Created by 凡柳松 on 17/5/4.
+//  Copyright © 2017年 凡柳松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestTravis/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestTravis
+//
+//  Created by 凡柳松 on 17/5/4.
+//  Copyright © 2017年 凡柳松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
